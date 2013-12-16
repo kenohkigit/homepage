6a21f414af73df6ca01112a45bd687849bc00de2
kenohkigit <git@ken-ohki.org>
2013-12-15 21:26:28 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title><?= isset($PageTitle) ? $PageTitle : "Ken-Ohki's Home"?></title>
+  </head>
+  <body>
+    <header id="header" class="inner"><h1><a href="/">Ken-Ohki</a></h1>
+      <nav id="main-nav">
+        <ul class="main">
+          <li><a href="/">Home</a></li>
+          <li><a href="resume.php/">Resume</a></li>
+          <li><a href="http://www.youtube.com/user/KenOhkisVid/" target="_blank" title="The Smoking Jacket Hour">SJH Youtube</a></li>
+          <li><a href="http://mc.ken-ohki.org/" target="_blank" title="The Smoking Jacke Hour Server">SJH Server</a>
+        </ul>
+      </nav>
+      <nav id="sub-nav" class="alignright">
+      	<div class="social">
+      		<a class="twitter" href="http://www.twitter.com/kenohki" target="_blank" title="Twitter">Twitter</a>
+      		<a class="google" href="http://google.com/+BrandonMerjil" target="_blank" title="Google+">Google+</a>
+      	</div>
+      </nav>
+    </header>
