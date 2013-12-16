e365aded4614a2a87713a9f42ea44d22d479ad7b
kenohkigit <git@ken-ohki.org>
2013-12-15 21:27:11 -0700

    Create resume.php

---
+++ resume.php
@@ -0,0 +1,68 @@
+<?php
+  $PageTitle="Ken-ohki's Resume";
+  include_once('header.php');
+?>
+  <div id="content" class="inner">
+    <h1>Brandon Merjil</h1>
+    51 S Criss Pl</ br>
+    Chandler, AZ 85226</ br>
+    623-505-6313</ br>
+    <a href="mailto:bmerjil@ken-ohki.org"</a>bmerjil@ken-ohki.org</ br>
+    <a href="http://ken-ohki.org"</a>ken-ohki.org</ br>
+
+    <h2>Education</h2>
+
+    DeVry University</ br>
+      Bachelor of Science, Computer Information Systems, Web Development and Administration, Phoenix, AZ October 2012</ br>
+
+    <h2>Work experience</h2>
+
+    <p><strong>Senior Linux System Administrator at Ebocom LLC in Phoenix, AZ - April 2005 to Current</strong></p>
+    <ul>
+      <li>24/7 system maintanace of data center Red Hat Enterprise Linux servers and user workstations.</li>
+      <li>Assist with new system research and purchasing.</li>
+      <li>Assist with new software research and purchasing for devlopemtn and production environments.</li>
+      <li>Part of PCI commitee within the office.</li>
+    </ul>
+
+    <p><strong>On-site technician at Red Seven in Tempe, AZ - January 2005 to March 2005</strong></p>
+    <ul>
+      <li>Handled consumer level computers, printers and network gear.</li>
+      <li>Diagnosis and repair comsumer equipment.</li>
+      <li>Software and hardware training for customers.</li>
+    </ul>
+
+    <p><strong>Data entry at PRIDE Industries, Intel University contract in Chandler, AZ - April 2004 to December 2004</strong></p>
+    <ul>
+      <li>Data entry for Intel University for continuing education of employees.</li>
+      <li>Input written answers from tests in to database for later review by instructors.</li>
+      <li>Created custom PDF that the students filled in. This was to help the document scanner have a higher rate of OCR.</li>
+    </ul>
+
+    <h2>Certifications</h2>
+    <ul>
+      <li>Red Hat Certified Engineer RHEL5 - May 2008 - 110-726-635</li>
+      <li>Snort Certified Professional - January 2008 - 4071662</li>
+      <li>A+ Service Technician Certified - April 1999 - COMP10150515</li>
+      <li>i-to-i TEFL certificate program 120 hr. - August 2013</li>
+    </ul>
+
+    <h2>Skills</h2>
+    <ul>
+      <li>Linux: IPTables, Apache, Bind, DHCP, NFS, OpenLDAP, Bash, Samba, Nginx</li>
+      <li>Database: MySql, Oracle, PostgreSQL, Sqlite</li>
+      <li>Programming: Eclipse, Java, Android, PHP</li>
+      <li>Storage: NetAPP, ONTAP, OnCommand, NFS, iSCSI</li>
+    </ul>
+    
+    <h2>Independent coursework</h2>
+    <ul>
+      <li>Begin Japanese Conversation I</li>
+      <li>Begin Japanese Conversation II</li>
+      <li>Elementary Japanese I</li>
+      <li>Elementary Japanese II</li>
+    </ul>
+  </div>
+<?php
+  include_once('footer.php');
+?>
