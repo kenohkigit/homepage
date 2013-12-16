48d952ad1d96f0bf431ce5700cb8966f91aeaeba
kenohkigit <git@ken-ohki.org>
2013-12-15 21:26:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  $PageTitle="Ken-ohki's Home";
+  include_once('header.php');
+?>
+  <div id="content" class="inner">
+  	<h4>2013/12/13 21:52MST</h4>
+  	Welcome to Ken-ohki.org<br />
+  	This is a rebuild of my old site and so it is a work in progress<br />
+  	Hopefully this basic body won't be here for very long.<br />
+  	Thank you for visiting.<br />
+  </div>
+<?php
+  include_once('footer.php');
+?>
