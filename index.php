4f378f6c5c88196bff40bc86ed938f1bbefcf12a
kenohkigit <git@ken-ohki.org>
2013-12-16 17:34:18 -0700

    Update index.php
    New entry.

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
   include_once('header.php');
 ?>
   <div id="content" class="inner">
+    <p><h4>2013/12/16 17:29MST</h4>
+    Well I seem to remember a bit on how to program in PHP.<br />
+    I'm going to work on the MySQL or PostgreSQL backend later.<br />
+  </div>
+  <div id="archive" class="inner">
   	<h4>2013/12/13 21:52MST</h4>
   	Welcome to Ken-ohki.org<br />
   	This is a rebuild of my old site and so it is a work in progress<br />
